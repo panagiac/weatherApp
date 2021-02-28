f86a1dffea01b46b3fc697b7f19449ac5789dbc5
panagiac <panagiac@protonmail.com>
2021-02-28 23:06:30 +0100

    Fragment Custom Transitions

--- presentation/src/main/java/com/panagiac/demo/weatherapp/extensions/FragmentActivityEx.kt
+++ presentation/src/main/java/com/panagiac/demo/weatherapp/extensions/FragmentActivityEx.kt
@@ -2,6 +2,7 @@ package com.panagiac.demo.weatherapp.extensions
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentTransaction
 import com.panagiac.demo.weatherapp.R
 
 fun FragmentActivity.replace(fragment: Fragment) {
@@ -14,6 +15,13 @@ fun FragmentActivity.replace(fragment: Fragment) {
 fun FragmentActivity.navigate(fragment: Fragment) {
     supportFragmentManager
         .beginTransaction()
+        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+        .setCustomAnimations(
+            R.anim.slide_in_from_right,
+            R.anim.slide_out_to_left,
+            R.anim.slide_in_from_left,
+            R.anim.slide_out_to_right
+        )
         .add(R.id.root, fragment)
         .addToBackStack(fragment.tag)
         .commit()
